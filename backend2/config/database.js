60e1402e645968187b9ca7c949ca7a5587f4f4f0
alejandroham <alejandro.mundaca.a@gmail.com>
2026-01-29 22:14:27 -0300

    Corrección en server.js, coreccion config

--- backend2/config/database.js
+++ backend2/config/database.js
@@ -1,15 +1,16 @@
 import pkg from "pg";
 const { Pool } = pkg;
-if (!process.env.PGPASSWORD) {
-  throw new Error("❌ PGPASSWORD NO está cargado");
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("❌ DATABASE_URL no está configurada");
 }
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: String(process.env.PGPASSWORD),
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
-
--- backend2/server.js
+++ backend2/server.js
@@ -4,5 +4,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`API corriendo en puerto ${PORT} http://localhost:${PORT}`);
+  console.log(`API corriendo en puerto ${PORT}`);
 });
