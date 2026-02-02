5d6a2424960518bca03a37813a1a4fb08ff17957
alejandroham <alejandro.mundaca.a@gmail.com>
2026-02-02 20:24:18 -0300

    fix: app.js para conexion local y vercel permite la conexón

--- backend2/app.js
+++ backend2/app.js
@@ -3,21 +3,33 @@ import cors from "cors";
 import { registerRoutes } from "./routes.js";
 import { logger } from "./middlewares/logger.js";
 
-
 const app = express();
 
-app.use(cors({
-  origin: "https://proyectoecommersefinal.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://proyectoecommersefinal.vercel.app"
+];
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Permite Postman / curl
+      if (!origin) return callback(null, true);
 
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 
 app.use(express.json());
 app.use(logger);
 
-app.use(cors());
 registerRoutes(app);
 
 export default app;
